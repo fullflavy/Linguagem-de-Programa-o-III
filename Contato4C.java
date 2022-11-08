f7093c30f025de4aaa1bb6fcf68539d093acec2a
fullflavy <114530865+fullflavy@users.noreply.github.com>
2022-11-08 15:08:50 -0300

    Create Contato4C.java
    Classe Cliente da classe Contato4.

---
+++ Contato4C.java
@@ -0,0 +1,7 @@
+public class Contato4C{
+	public static void main(String[]args){
+		
+		Contato4 contato = new Contato4("Ana", "ana@gmail.com", "8854-3245", 28, 2, 2000);
+		contato.imprimirContato();
+	}
+}
