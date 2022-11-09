9796f6a36994f53a0930c2a59e380de9c0f06a5b
fullflavy <114530865+fullflavy@users.noreply.github.com>
2022-11-09 14:58:43 -0300

    Lamp.java
    lista 01: Inclua no modelo Lampada (Exercício 1) uma operação estaLigada que retorne
    verdadeiro se a lâmpada estiver ligada e falso caso contrário.

---
+++ Lamp.java
@@ -0,0 +1,42 @@
+class Lamp {
+  private int lampStatus = 0;
+
+  private boolean verLamptatus(int lampStatus){  //se a lampada esta acesa retorna verdadeiro senao falso.
+    if(lampStatus == 1){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  private void error(){
+    System.out.println("Error!!");
+    System.exit(-1);
+  }
+  
+  public int ligthUp() {
+    if(verLamptatus(lampStatus)){  //trata se a lampada ja esta acesa.
+      error();
+    }else{
+      lampStatus = 1;
+    }
+    return lampStatus;
+  }
+
+  public int toSwitchOff() {
+    if(!verLamptatus(lampStatus)){ //trata se a lampada ja esta apagada.
+      error();
+    }else{
+      lampStatus = 0;
+    }
+    return lampStatus;
+  }
+
+  public void printLampStatus(){
+    if(verLamptatus(lampStatus)){
+      System.out.println("\nThe lamp is on.\n");
+    }else{
+      System.out.println("\nThe lamp is off.\n");
+    }
+  }
+}
