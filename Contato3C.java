6181c5edb51e7d63a993947fa6014811316a1569
fullflavy <114530865+fullflavy@users.noreply.github.com>
2022-11-09 13:59:52 -0300

    Contato3C.java
    lista 03: Classe cliente da classe Contato3.

---
+++ Contato3C.java
@@ -0,0 +1,7 @@
+public class Contato3C{
+	public static void main(String[]args){
+		
+		Contato3 contato = new Contato3("Ana", "ana@gmail.com", "8854-3245", 28, 2, 2000);
+		contato.imprimirContato();
+	}
+}
