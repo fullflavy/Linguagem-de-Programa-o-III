981944e23e55f1a3ffc1d06d8f2eb87f5241751b
fullflavy <114530865+fullflavy@users.noreply.github.com>
2022-12-04 15:28:49 -0300

    Update Main.java

--- lista07/exe002/heranca/Main.java
+++ lista07/exe002/heranca/Main.java
@@ -1,7 +1,9 @@
-public class Main {
+public class Main{
     public static void main(String[] args){
-        Ponto3D ponto = new Ponto3D(1, 1, 1);
+        Ponto3D p = new Ponto3D(1, 1, 1);
+        Ponto3D p1 = new Ponto3D(1, 2, 3);
 
-        System.out.println(ponto.toString());
+        System.out.println(p.toString());
+        System.out.println(p.distancia(p1));
     }
 }
