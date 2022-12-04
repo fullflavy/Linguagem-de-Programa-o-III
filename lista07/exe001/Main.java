1aa4610cba2abcd1324cff0dc0946b44fafbcd60
fullflavy <114530865+fullflavy@users.noreply.github.com>
2022-12-04 15:31:03 -0300

    Update Main.java

--- lista07/exe001/Main.java
+++ lista07/exe001/Main.java
@@ -1,3 +1,9 @@
-public class Main {
-    
+public class Main{
+    public static void main(String[] args){
+        Ponto2D p = new Ponto2D(1, 2);
+        Ponto2D p1 = new Ponto2D(2, 2);
+
+        System.out.println(p.toString());
+        System.out.println(p.distancia(p1));
+    }
 }
